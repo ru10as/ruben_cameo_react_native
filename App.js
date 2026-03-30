7b817b0e5470a9d5c35bc8b6a1a4357a8e8076fe
Rubén <ruben81003@gmail.com>
2026-03-30 11:51:21 +0200

    Componentes React Native

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'; 
+import React from 'react'; 
+import { View } from 'react-native'; 
+import { SafeAreaProvider } from 'react-native-safe-area-context'; 
+import { Provider as PaperProvider } from 'react-native-paper'; 
+import Campobase from './componentes/CampobaseComponent'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() { 
+  return ( 
+    <SafeAreaProvider> 
+      <PaperProvider> 
+        <View style={{ flex: 1 }}> 
+        <Campobase /> 
+        <StatusBar style="auto" /> 
+        </View> 
+      </PaperProvider> 
+    </SafeAreaProvider> 
+  ); 
+  } 
---
+++ componentes/CalendarioComponent.js
@@ -0,0 +1,63 @@
+import React from 'react'; 
+import { FlatList, View, Image, StyleSheet } from 'react-native'; 
+import { SafeAreaView } from 'react-native-safe-area-context'; 
+import { List, Divider } from 'react-native-paper'; 
+ 
+function Calendario({ excursiones }) { 
+  const renderCalendarioItem = ({ item }) => { 
+    return ( 
+      <View> 
+        <List.Item 
+          title={item.nombre} 
+          description={item.descripcion} 
+          titleNumberOfLines={0} 
+          descriptionNumberOfLines={6} 
+          left={(props) => ( 
+            <Image 
+              source={require('./imagenes/40Años.png')} 
+              style={[props.style, styles.imagen]} 
+              resizeMode="cover" 
+            /> 
+          )} 
+          titleStyle={styles.titulo} 
+          descriptionStyle={styles.descripcion} 
+          contentStyle={styles.contenido} 
+        /> 
+        <Divider /> 
+      </View> 
+    ); 
+  }; 
+ 
+  return ( 
+    <SafeAreaView style={styles.container}> 
+      <FlatList 
+        data={excursiones} 
+        renderItem={renderCalendarioItem} 
+        keyExtractor={(item) => item.id.toString()} 
+      /> 
+    </SafeAreaView> 
+  ); 
+} 
+
+const styles = StyleSheet.create({ 
+  container: { 
+    flex: 1, 
+  }, 
+  imagen: { 
+    width: 40, 
+    height: 40, 
+    alignSelf: 'center', 
+  }, 
+  contenido: { 
+    paddingRight: 8, 
+  }, 
+  titulo: { 
+    fontSize: 16, 
+  }, 
+  descripcion: { 
+    fontSize: 14, 
+    lineHeight: 20, 
+  }, 
+}); 
+ 
+export default Calendario; 
